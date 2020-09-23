b962d528089780c126ed2fe6e82bc8b4c469380a
shan5742 <asamshan456@gmail.com>
2020-09-23 13:41:05 +0100

    initialising websockets and setting everything up by adding script tags to html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on("connection", () => {
+  console.log("New websocket connection");
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
